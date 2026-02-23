f5361a5e45a79144be2f6118e1d6e5f66a109fe0
AfiqIzzat07 <afiqizzat1105@gmail.com>
2026-02-23 14:16:49 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBD990zggrFpi5Z2DwAOLShTjJUDN8ydBo",
+  authDomain: "smart-dustbin-150307.firebaseapp.com",
+  databaseURL: "https://smart-dustbin-150307-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "smart-dustbin-150307",
+  storageBucket: "smart-dustbin-150307.firebasestorage.app",
+  messagingSenderId: "470186830441",
+  appId: "1:470186830441:web:9355f4e1c10e498b26d0f8"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
