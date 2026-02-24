02e42b70ba61770b88bb7994763e81a0948ab4cd
AfiqIzzat07 <afiqizzat1105@gmail.com>
2026-02-24 16:28:28 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,13 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
-
-export const firebaseConfig = {
+// Replace with your Firebase config
+const firebaseConfig = {
   apiKey: "AIzaSyBD990zggrFpi5Z2DwAOLShTjJUDN8ydBo",
-  databaseURL: "https://smart-dustbin-150307-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  authDomain: "smart-dustbin-150307.firebaseapp.com",
+  databaseURL: "https://smart-dustbin-150307-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "smart-dustbin-150307",
+  storageBucket: "smart-dustbin-150307.appspot.com",
+  messagingSenderId: "470186830441",
+  appId: "1:470186830441:web:9355f4e1c10e498b26d0f8"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
